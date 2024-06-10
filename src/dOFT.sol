e70acb9d40b3c85643bde3f5000c9761a2d9fc13
jdorado <juancamilo.dorado@gmail.com>
2024-06-10 14:54:03 +0400

    wip

---
+++ src/dOFT.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {OFT} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFT.sol";
+
+contract dOFT {
+    constructor(){
+
+    }
+}
