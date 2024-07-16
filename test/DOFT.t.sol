1fb25e4373fb20a186a7c7d705fb5d0efc1cd10e
Gulam Rasul Shah <99994920+grsLammy@users.noreply.github.com>
2024-07-16 20:41:25 +0530

    feat(test): fix forge-std import path

--- test/DOFT.t.sol
+++ test/DOFT.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-import { Test } from "lib/forge-std/src/Test.sol";
+import { Test } from "forge-std/Test.sol";
 import { DOFT } from "../src/DOFT.sol";
 
 import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
