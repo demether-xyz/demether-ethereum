cc4b87d30483d125faf9f99eef3c14f776488566
jdorado <juancamilo.dorado@gmail.com>
2024-08-09 07:26:56 -0600

    RES-06: Minter is allowed to be zero address

--- src/DOFT.sol
+++ src/DOFT.sol
@@ -22,7 +22,6 @@ import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils
 contract DOFT is OFTUpgradeable, UUPSUpgradeable {
     error ImplementationIsNotContract(address newImplementation);
     error UnauthorizedMinter(address caller);
-    error InvalidAddress();
 
     address private _minter;
 
@@ -37,13 +36,6 @@ contract DOFT is OFTUpgradeable, UUPSUpgradeable {
         _;
     }
 
-    /// @notice Assigns a new minter to ensure security, if previous minter get compromised
-    /// @param _newMinter New minter address
-    function setMinter(address _newMinter) external onlyOwner {
-        if (_newMinter == address(0)) revert InvalidAddress();
-        _minter = _newMinter;
-    }
-
     /// @notice Initializes the contract with token name, symbol, initial delegate, and minter.
     /// @param _name Token name.
     /// @param _symbol Token symbol.
@@ -55,7 +47,6 @@ contract DOFT is OFTUpgradeable, UUPSUpgradeable {
         address _delegate,
         address _minterAddress
     ) external initializer onlyProxy {
-        if (_minterAddress == address(0)) revert InvalidAddress();
         __OFT_init(_name, _symbol, _delegate);
         __Ownable_init();
         _transferOwnership(_delegate);
@@ -78,6 +69,12 @@ contract DOFT is OFTUpgradeable, UUPSUpgradeable {
     function burn(address _from, uint256 _amount) external onlyMinter returns (bool) {
         _burn(_from, _amount);
         return true;
+    }
+
+    /// @notice Assigns a new minter address.
+    /// @param _newMinter New minter address
+    function setMinter(address _newMinter) external onlyOwner {
+        _minter = _newMinter;
     }
 
     /// @dev Authorizes the upgrade of the contract.
